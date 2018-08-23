de17187bb9d6ca0f7e408c318e6d7b95076dc8e1
zoutttttt <893989551@qq.com>
2018-08-23 12:13:05 +0800

    ssbbb

--- OFDM_FINAL/cyclic_prefix.m
+++
@@ -1,5 +0,0 @@
-% This function adds the last 36 bits of the symbol to the beginning as cyclic prefix 
-function symbol_cp=cyclic_prefix(output_ifft)
-    symbol_cp=[output_ifft(477:512); output_ifft];
-   
-end
--- OFDM_FINAL/time.m
+++
@@ -1,8 +0,0 @@
-% This function plots the time domain response
-function []= time(inp,titled,col)
-    stem(inp,col);
-    title(titled);
-    xlabel('index');
-    ylabel('amplitude');
-    axis tight;
-end
